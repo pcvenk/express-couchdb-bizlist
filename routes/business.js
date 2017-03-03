8f55a47823eeb045381f821fe55b8565fd4b87de
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-03-03 11:30:29 +0100

    Added the create new business template

--- routes/business.js
+++ routes/business.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+//INDEX ROUTE
+router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
+
+//ADD ROUTE
+router.get('/add', function(req, res) {
+  res.render('addBusiness');
+});
+
+
 
 module.exports = router;
